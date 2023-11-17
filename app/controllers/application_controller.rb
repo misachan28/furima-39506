6be5020df8e8a69a05fc4c5a45d19ebec043f39f
misachan28 <misatech33@gmail.com>
2023-11-17 17:24:27 +0900

    ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,5 @@
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:last_name,:first_name_kana,:last_name_kana,:birthday])
   end
-  
+
 end
