66073231f0004519a0b2c9773226ee5669ee9d43
misachan28 <misatech33@gmail.com>
2023-11-17 21:22:07 +0900

    userモデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
         
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :password_confirmation, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
 end
+
