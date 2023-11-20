91f26b8c0b181e6cd988a822141001603b2abfa5
misachan28 <misatech33@gmail.com>
2023-11-20 21:31:35 +0900

    出品機能　firstcommit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,39 @@
 class ItemsController < ApplicationController
-  
+  before_action :authenticate_user!
+
   def index
+    @items = Item.all
   end
 
+  def new
+    @item = Item.new
+    @category = Category.all
+    @condition = Condition.all
+    @postage = Postage.all
+    @prefecture = Prefecture.all
+    @shipping_date = ShippingDate.all
+
+  end
+
+  def create
+    @category = Category.all
+    @condition = Condition.all
+    @postage = Postage.all
+    @prefecture = Prefecture.all
+    @shipping_date = ShippingDate.all
+    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:nickname,:explanation,:category_id,:condition_id,:postage_id,:prefecture_id,:shipping_date_id, :price).merge(user_id: current_user.id)
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,23 @@
+require 'active_hash'
+
+class Item < ApplicationRecord
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  belongs_to :user
+
+  validates :image, presence: true
+  validates :nickname, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :category_id, numericality: { other_than: 1 ,  message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" }
+
+end
